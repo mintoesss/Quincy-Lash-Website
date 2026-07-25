60fc8fbfc65d40354e32c8b2dd7b13b15d0cabff
Minh <minhhhle04@gmail.com>
2026-07-24 19:28:20 -0700

    raged quit, not sure what i added on views.py

--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Service, Client, Appointment
 
+
+def service_list(request):
+    if request.method == "POST":
+        # read the submitted data
+        # create the Client
+        # create the Appointment
+        return redirect("service_list")
 # Breaking down Service.objects.filter(is_active=True)
 # Service = model class / table
 # objects = the model's manager, every model gets one, entire job is fetching, filtering, and counting rows
 # .filter(is_active=True) = return the rows where is_active column is True
 # store it in a variable called services, to be used in an html page
-def service_list(request):
     services = Service.objects.filter(is_active=True)
     return render(request, "website/index.html", {
         "services": services,
