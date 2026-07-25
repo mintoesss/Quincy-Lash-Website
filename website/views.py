12c9b781127b541ef2a6707abe317402cc9ab4ec
Minh <minhhhle04@gmail.com>
2026-07-24 19:04:21 -0700

    add service list: views.py urls.py and index.html

--- website/views.py
+++ website/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Service, Client, Appointment
 
-# Create your views here.
+# Breaking down Service.objects.filter(is_active=True)
+# Service = model class / table
+# objects = the model's manager, every model gets one, entire job is fetching, filtering, and counting rows
+# .filter(is_active=True) = return the rows where is_active column is True
+# store it in a variable called services, to be used in an html page
+def service_list(request):
+    services = Service.objects.filter(is_active=True)
+    return render(request, "website/index.html", {
+        "services": services,
+    })
